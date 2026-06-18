b77a070b852095413995609a0bbca21670ca2aa3
yogeshtawde <ytawde@belenits.in>
2026-06-18 16:37:04 +0530

    first springboot demo app

---
+++ First/src/main/java/org/example/first/DemoService.java
@@ -0,0 +1,13 @@
+package org.example.first;
+
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class DemoService
+{
+    public String sayHeloo()
+    {
+        return  "Hello";
+    }
+}
